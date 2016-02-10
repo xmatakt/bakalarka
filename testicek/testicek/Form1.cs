02746c73144820bb68e8d00919334863dd55c723
robertspir <spir.robert@gmail.com>
2016-02-10 11:40:04 +0100

    dsasad

--- testicek/testicek/Form1.cs
+++ testicek/testicek/Form1.cs
@@ -19,7 +19,7 @@ namespace testicek
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("tralalaaaa");
+            MessageBox.Show("tralalaaadsadsadasa");
         }
     }
 }
